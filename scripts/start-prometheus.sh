465b0316e0e8536f6b6a8ccf89df0bcf131d615d
Danish Amjad <danishamjad30@gmail.com>
2020-07-13 14:55:00 +0200

    added rescale skeleton.

--- scripts/start-prometheus.sh
+++ scripts/start-prometheus.sh
@@ -2,6 +2,7 @@
 source "`dirname $0`/environment.sh"
 ${PROMETHEUS_HOME}/prometheus \
     --config.file=${PROMETHEUS_HOME}/prometheus.yml \
+    --web.listen-address="0.0.0.0:9091" \
     --web.enable-admin-api \
     --storage.tsdb.path=${PROMETHEUS_HOME}/data > ${PROMETHEUS_HOME}/logs 2>&1 &
 echo $!> ${PROMETHEUS_HOME}/pid
