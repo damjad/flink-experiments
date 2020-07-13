c81107006ee6f283fc5e6a169ffd88646cb6ab53
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 00:47:34 +0200

    test with different config.

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -3,7 +3,6 @@
 HOME="/share/hadoop/danish/flink-experiments/"
 
 export KAFKA_HOME="$HOME/binaries/kafka"
-export KAFKA_HOME_1="$HOME/binaries/kafka_1"
 export FLINK_HOME="$HOME/builds/incremental-migration/"
 export PROMETHEUS_HOME="$HOME/binaries/prometheus-2.16.0.linux-386"
 
--- scripts/stop-kafka_1.sh
+++ scripts/stop-kafka_1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
 
-${KAFKA_HOME_1}/bin/kafka-server-stop.sh
+${KAFKA_HOME}/bin/kafka-server-stop.sh
