0dd1d8d900d3a977ac4091b92ee396661fa41dde
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 00:13:16 +0200

    fixed prometheus path

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -5,7 +5,7 @@ HOME="../`dirname $0`"
 export KAFKA_HOME="$HOME/binaries/kafka"
 export KAFKA_HOME_1="$HOME/binaries/kafka_1"
 export FLINK_HOME="$HOME/builds/incremental-migration/"
-export PROMETHEUS_HOME="$HOME/prometheus-2.16.0.linux-386"
+export PROMETHEUS_HOME="$HOME/binaries/prometheus-2.16.0.linux-386"
 
 export N_PARTITIONS=10
 export TOPIC=words
