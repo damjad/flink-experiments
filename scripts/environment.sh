2393f54dbfc19367c1b1e6ee5ebb583910243af0
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 01:40:03 +0200

    Updated generator scripts

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -2,9 +2,10 @@
 #change this to the place where kafka is in your PC
 export HOME="/share/hadoop/danish/flink-experiments/"
 
+export PROMETHEUS_HOME="$HOME/binaries/prometheus-2.16.0.linux-386"
 export KAFKA_HOME="$HOME/binaries/kafka"
 export RHINO_HOME="$HOME/build/incremental-migration/"
-export PROMETHEUS_HOME="$HOME/binaries/prometheus-2.16.0.linux-386"
+export MORPHEUS_HOME=${RHINO_HOME}/morpheus-tests
 
 export N_PARTITIONS=10
 export TOPIC=words
--- scripts/start-generator.sh
+++ scripts/start-generator.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
 #
-MORPHEUS_HOME=${RHINO_HOME}/morpheus-tests
-
 java -cp $MORPHEUS_HOME/src/main/conf:$MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar \
       org.apache.flink.morpheus.wordcount.kafka.CustomStringKafkaGenerator \
       -properties-file $HOME/configs/load-balance-skew-word-count.properties > /data/1/peel/logs 2>&1 &
