4fde109be002a1f6f4b8c2af43d21e27052830a7
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 01:14:02 +0200

    Added configs for the generator.

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #change this to the place where kafka is in your PC
-HOME="/share/hadoop/danish/flink-experiments/"
+export HOME="/share/hadoop/danish/flink-experiments/"
 
 export KAFKA_HOME="$HOME/binaries/kafka"
-export FLINK_HOME="$HOME/builds/incremental-migration/"
+export RHINO_HOME="$HOME/builds/incremental-migration/"
 export PROMETHEUS_HOME="$HOME/binaries/prometheus-2.16.0.linux-386"
 
 export N_PARTITIONS=10
---
+++ scripts/stop-generator.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source "`dirname $0`/environment.sh"
+kill `cat $MORPHEUS_HOME/gen-pid`
