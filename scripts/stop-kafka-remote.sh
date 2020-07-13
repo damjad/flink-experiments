c29a98240b979d3de68f9d35460f3b93687ece62
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 01:45:11 +0200

    updated scripts for startup-logs consistency

--- scripts/stop-kafka-remote.sh
+++ scripts/stop-kafka-remote.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pdsh -w cloud-13 '/share/hadoop/danish/flink-experiments/scripts/stop-kafka.sh > /data/1/peel/kafka-startup.log 2>&1 &'
-pdsh -w cloud-14 '/share/hadoop/danish/flink-experiments/scripts/stop-kafka_1.sh > /data/1/peel/kafka-startup.log 2>&1 &'
+pdsh -w cloud-13 '/share/hadoop/danish/flink-experiments/scripts/stop-kafka.sh >> /data/1/peel/kafka-startup.log 2>&1 &'
+pdsh -w cloud-14 '/share/hadoop/danish/flink-experiments/scripts/stop-kafka_1.sh >> /data/1/peel/kafka-startup.log 2>&1 &'
--- scripts/stop-zookeper-remote.sh
+++ scripts/stop-zookeper-remote.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pdsh -w cloud-12 '/share/hadoop/danish/flink-experiments/scripts/stop-zookeeper.sh'
+pdsh -w cloud-12 '/share/hadoop/danish/flink-experiments/scripts/stop-zookeeper.sh >> /data/1/peel/zookeeper-startup.log 2>&1 &'
