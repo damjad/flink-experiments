ff77c47990626edf2b728707867735c7cfa7860e
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 02:49:20 +0200

    added class and jvm args for flink run

--- scripts/word-count-rhino-run.sh
+++ scripts/word-count-rhino-run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
 
-$RHINO_HOME/build-target/bin/flink run -m $RHINO_ADDRESS -q $MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar
+$RHINO_HOME/build-target/bin/flink run -m $RHINO_ADDRESS \
+    -c org.apache.flink.morpheus.wordcount.SkewWordCount \
+    -q $MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar \
+    --properties-file $HOME/configs/load-balance-skew-word-count.properties
