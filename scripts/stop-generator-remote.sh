b8f9e139fdedf4703ca4bbfe70ab3dd79cbdbda1
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 02:08:37 +0200

    Added remote generator start-stop

---
+++ scripts/stop-generator-remote.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pdsh -w cloud-16 '/share/hadoop/danish/flink-experiments/scripts/stop-generator.sh.sh >> /data/1/peel/gen-startup.log 2>&1 &'
+pdsh -w cloud-17 '/share/hadoop/danish/flink-experiments/scripts/stop-generator.sh.sh >> /data/1/peel/gen-startup.log 2>&1 &'
