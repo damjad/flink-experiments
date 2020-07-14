1caa7aeba156352e7a0bf7a6be63ef9119a49eac
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 22:00:16 +0200

    zookeeper pid

--- scripts/start-zookeeper.sh
+++ scripts/start-zookeeper.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
-${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties
+${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties $HOME/logs/zookeeper-startup.log 2>&1 &
+echo $!> $HOME/pids/zookeeper-pid
