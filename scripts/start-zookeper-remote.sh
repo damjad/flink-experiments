a3512bb5e88f14a20971754aab852fbe38f5f822
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 22:12:47 +0200

    xx

--- scripts/start-zookeper-remote.sh
+++ scripts/start-zookeper-remote.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pdsh -w cloud-12 '/share/hadoop/danish/flink-experiments/scripts/start-zookeeper.sh > $HOME/logs/zookeeper-startup.log 2>&1 &'
+pdsh -w cloud-12 '/share/hadoop/danish/flink-experiments/scripts/start-zookeeper.sh'
