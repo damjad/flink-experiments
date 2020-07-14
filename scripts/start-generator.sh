2b64dff75d6f7ed4e7eb5ad295ffb2ea437b307b
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 03:56:27 +0200

    fix gen pid file path

--- scripts/start-generator.sh
+++ scripts/start-generator.sh
@@ -4,4 +4,4 @@ source "`dirname $0`/environment.sh"
 java -cp $MORPHEUS_HOME/src/main/conf:$MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar \
       org.apache.flink.morpheus.wordcount.kafka.CustomStringKafkaGenerator \
       -properties-file $HOME/configs/load-balance-skew-word-count.properties > /data/1/peel/gen-startup.log 2>&1 &
-echo $!> $MORPHEUS_HOME/gen-pid
+echo $!> /data/1/peel/gen-pid
