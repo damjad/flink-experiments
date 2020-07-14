4dbdea3daded0eadbee119d3f0c556c501fdab9b
Danish Amjad <danishamjad30@gmail.com>
2020-07-14 22:43:26 +0200

    log level trace for generator.

--- scripts/start-generator.sh
+++ scripts/start-generator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
 #
-java -cp $MORPHEUS_HOME/src/main/conf:$MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar \
+java -cp $HOME/configs:$MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar \
       org.apache.flink.morpheus.wordcount.kafka.CustomStringKafkaGenerator \
       $HOME/configs/load-balance-skew-word-count.properties > $HOME/logs/gen-startup.log 2>&1 &
 echo $!> $HOME/pids/gen-pid
