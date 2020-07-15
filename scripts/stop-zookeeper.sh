136dd824c937502755c5a4bb8ce9c2ff0945fabe
Danish Amjad <danishamjad30@gmail.com>
2020-07-15 04:32:00 +0200

    xx

--- scripts/stop-zookeeper.sh
+++ scripts/stop-zookeeper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
-${KAFKA_HOME}/bin/zookeeper-server-stop.sh ${KAFKA_HOME}/config/zookeeper.properties
+${KAFKA_HOME}/bin/zookeeper-server-stop.sh
