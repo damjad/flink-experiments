c0845fc08493b0c8285205df85c56448af51bad0
Danish Amjad <danishamjad30@gmail.com>
2020-07-20 22:19:08 +0200

    dstat date etc.

--- scripts/start-dstat.sh
+++ scripts/start-dstat.sh
@@ -2,9 +2,10 @@
 source "`dirname $0`/environment.sh"
 
 #be careful with the following it depends on RAM.
-OUT_FILE=$HOME/test-data/morpheus-tests/dstat-`hostname`-`date`.csv
 HOST_NAME=`hostname`
-python $DSTAT_HOME/dstat -cdgmnrpryl --mem-adv -C 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,total -N eth0 --epoch --noheaders --vm-adv --aio  --bw --nocolor --output $OUT_FILE 5 > /dev/null 2>&1 &
+DATE=`date '+%Y-%m-%d-%H:%H:%S'`
+OUT_FILE=$HOME/test-data/morpheus-tests/dstat-$HOST_NAME-$DATE.csv
+echo "python $DSTAT_HOME/dstat -cdgmnrpryl --mem-adv -C 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,total -N eth0 --epoch --noheaders --vm-adv --aio  --bw --nocolor --output $OUT_FILE 5 > /dev/null 2>&1 &"
 pid=$!
 
 echo $pid > $HOME/pids/dstat-$HOST_NAME.pid
