c040ab69482086a48a7aac6504db7d28ecbc058f
Danish Amjad <danishamjad30@gmail.com>
2020-07-20 21:56:04 +0200

    updated pid dir for dstat

--- scripts/start-dstat.sh
+++ scripts/start-dstat.sh
@@ -7,4 +7,4 @@ OUT_FILE=$HOME/test-data/morpheus-tests/dstat-`hostname`-`date`.csv
 python $DSTAT_HOME/dstat -cdgmnrpryl --mem-adv -C 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,total -N eth0 --epoch --noheade    rs --vm-adv --aio  --bw --nocolor --output $OUT_FILE 5 > /dev/null 2>&1 &
 pid=$!
 
-echo $pid > $DSTAT_HOME/pids/dstat-`hostname`.pid
+echo $pid > $HOME/pids/dstat-`hostname`.pid
