efa47a6daf4227fb6b936476a1a73b2691e32623
Danish Amjad <danishamjad30@gmail.com>
2020-07-20 21:58:47 +0200

    Added dstat log

--- scripts/start-dstat.sh
+++ scripts/start-dstat.sh
@@ -4,7 +4,7 @@ source "`dirname $0`/environment.sh"
 #be careful with the following it depends on RAM.
 OUT_FILE=$HOME/test-data/morpheus-tests/dstat-`hostname`-`date`.csv
 
-python $DSTAT_HOME/dstat -cdgmnrpryl --mem-adv -C 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,total -N eth0 --epoch --noheade    rs --vm-adv --aio  --bw --nocolor --output $OUT_FILE 5 > /dev/null 2>&1 &
+python $DSTAT_HOME/dstat -cdgmnrpryl --mem-adv -C 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,total -N eth0 --epoch --noheade    rs --vm-adv --aio  --bw --nocolor --output $OUT_FILE 5 > $HOME/logs/dstat-`hostname`.log 2>&1 &
 pid=$!
 
 echo $pid > $HOME/pids/dstat-`hostname`.pid
