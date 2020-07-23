16eb7fdc8c786dff0bd075501af7e98888f14e14
Danish Amjad <danishamjad30@gmail.com>
2020-07-23 17:45:47 +0200

    added start-dstat script

---
+++ scripts/start-services-vanilla.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source "`dirname $0`/environment.sh"
+cd "`dirname $0`"
+bash start-prometheus.sh &
+sleep 5
+bash start-zookeper-remote.sh &
+sleep 5
+bash start-kafka-remote.sh &
+sleep 5
+bash start-vanilla-flink.sh &
