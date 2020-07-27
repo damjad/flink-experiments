07038f232a415c1f9eba32cbb79adc29f14d62a5
Danish Amjad <danishamjad30@gmail.com>
2020-07-27 03:35:22 +0200

    increased parallelism after the restart.

--- scripts/word-count-vanilla-run-with-savepoint.sh
+++ scripts/word-count-vanilla-run-with-savepoint.sh
@@ -2,7 +2,7 @@
 source "`dirname $0`/environment.sh"
 
 SAVE_FILE=`cat $HOME/pids/word_count-fid`
-OUTPUT=$($VANILLA_HOME/build-target/bin/flink run -s $SAVE_FILE -d $VANILLA_HOME/morpheus-tests-vanilla/target/morpheus-tests-vanilla-1.6.0.jar --properties-file /share/hadoop/danish/flink-experiments//configs/vanilla-load-balance-skew-word-count.properties)
+OUTPUT=$($VANILLA_HOME/build-target/bin/flink run -s $SAVE_FILE -d $VANILLA_HOME/morpheus-tests-vanilla/target/morpheus-tests-vanilla-1.6.0.jar --properties-file /share/hadoop/danish/flink-experiments//configs/vanilla-load-balance-skew-word-count_1.properties)
 echo $OUTPUT
 
 echo $OUTPUT | grep -oP 'JobID \K\w+' > $HOME/pids/word_count-jid
