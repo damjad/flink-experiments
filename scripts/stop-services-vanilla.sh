a4fb7801cd323708828dc2679fc7d848e6e718ff
Danish Amjad <danishamjad30@gmail.com>
2020-07-29 03:10:58 +0200

    ...

--- scripts/stop-services-vanilla.sh
+++ scripts/stop-services-vanilla.sh
@@ -5,7 +5,7 @@ cd "`dirname $0`"
 bash stop-vanilla-flink.sh &
 sleep 5
 bash stop-kafka-remote.sh &
-sleep 5
+sleep 20
 bash stop-zookeper-remote.sh &
 sleep 5
 bash stop-prometheus.sh
--- scripts/stop-services.sh
+++ scripts/stop-services.sh
@@ -5,7 +5,7 @@ cd "`dirname $0`"
 bash stop-rhino.sh &
 sleep 5
 bash stop-kafka-remote.sh &
-sleep 5
+sleep 20
 bash stop-zookeper-remote.sh &
 sleep 5
 bash stop-prometheus.sh
