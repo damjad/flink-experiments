fe9ffa2888c765971884a4da372cf79f6b8f204f
Danish Amjad <danishamjad30@gmail.com>
2020-07-30 22:01:34 +0200

    updated the jar for rhino word count.

--- scripts/word-count-run-with-savepoint.sh
+++ scripts/word-count-run-with-savepoint.sh
@@ -2,7 +2,7 @@
 source "`dirname $0`/environment.sh"
 
 SAVE_FILE=`cat $HOME/pids/word_count-fid`
-OUTPUT=$($RHINO_HOME/build-target/bin/flink run -s $SAVE_FILE -d $RHINO_HOME/morpheus-tests/target/morpheus-tests-1.6.0.jar --properties-file /share/hadoop/danish/flink-experiments//configs/load-balance-skew-word-count.properties)
+OUTPUT=$($RHINO_HOME/build-target/bin/flink run -s $SAVE_FILE -d $MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar  --properties-file /share/hadoop/danish/flink-experiments//configs/load-balance-skew-word-count.properties)
 echo $OUTPUT
 
 echo $OUTPUT | grep -oP 'JobID \K\w+' > $HOME/pids/word_count-jid
--- scripts/word-count-run.sh
+++ scripts/word-count-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source "`dirname $0`/environment.sh"
 
-OUTPUT=$($RHINO_HOME/build-target/bin/flink run -d $RHINO_HOME/morpheus-tests/target/morpheus-tests-1.6.0.jar --properties-file /share/hadoop/danish/flink-experiments//configs/load-balance-skew-word-count.properties)
+OUTPUT=$($RHINO_HOME/build-target/bin/flink run -d $MORPHEUS_HOME/target/morpheus-tests-1.6-Migration.jar  --properties-file /share/hadoop/danish/flink-experiments/configs/load-balance-skew-word-count.properties)
 echo $OUTPUT
 
 echo $OUTPUT | grep -oP 'JobID \K\w+' > $HOME/pids/word_count-jid
