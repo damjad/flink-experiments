b52876abfb05fde5318d8e0c49cbebbf50035136
Danish Amjad <danishamjad30@gmail.com>
2020-07-30 22:10:53 +0200

    removed savepoint logic from rhino.

--- scripts/word-count-stop.sh
+++ scripts/word-count-stop.sh
@@ -3,7 +3,7 @@ source "`dirname $0`/environment.sh"
 
 mkdir -p $RHINO_HOME/savepoints
 JOB_ID=`cat $HOME/pids/word_count-jid`
-OUTPUT=$($RHINO_HOME/build-target/bin/flink cancel -s $RHINO_HOME/savepoints $JOB_ID)
+OUTPUT=$($RHINO_HOME/build-target/bin/flink cancel $JOB_ID)
 
 echo $OUTPUT
 
