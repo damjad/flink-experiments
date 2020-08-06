ffab105bc1f57eb9b251aa48699af4bb82cedd68
Danish Amjad <danishamjad30@gmail.com>
2020-08-07 00:03:43 +0200

    updated sustainable throughput. now performing the experiments.

--- scripts/start-sustainable-vanilla-scenario.sh
+++ scripts/start-sustainable-vanilla-scenario.sh
@@ -2,8 +2,25 @@
 source "`dirname $0`/environment.sh"
 #
 
-for (( c=100; c<=2300; c=c+25))
+c=300
+sed -i "s/\(^source-rate=\).*/\1$c/" $HOME/configs/vanilla-sustainable-throughput.properties
+./stop-generator-remote.sh && sleep 0.5 && ./start-sustainable-vanilla-remote.sh && sleep 30
+
+
+for (( c=1700; c<=2225; c=c+25))
 do
-#./stop-generator-remote.sh && ./start-sustainable-vanilla-remote.sh && sleep 30 && \
-sed -i 's/\(^source-rate=\).*/\1${c}/' $HOME/configs/vanilla-sustainable-throughput.properties
+sed -i "s/\(^source-rate=\).*/\1$c/" $HOME/configs/vanilla-sustainable-throughput.properties
+./stop-generator-remote.sh && sleep 0.5 && ./start-sustainable-vanilla-remote.sh && sleep 20
 done
+
+
+for (( c=2250; c<=2300; c=c+25))
+do
+sed -i "s/\(^source-rate=\).*/\1$c/" $HOME/configs/vanilla-sustainable-throughput.properties
+./stop-generator-remote.sh && ./start-sustainable-vanilla-remote.sh && sleep 50
+done
+
+c=2190
+sed -i "s/\(^source-rate=\).*/\1$c/" $HOME/configs/vanilla-sustainable-throughput.properties
+./stop-generator-remote.sh && sleep 10 && ./start-sustainable-vanilla-remote.sh && sleep 300 &&\
+./stop-generator-remote.sh && ./word-count-vanilla-stop-with-savepoint.sh
