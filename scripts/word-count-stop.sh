501597dd448c12c381851bcc47d6384bfd3b430e
Danish Amjad <danishamjad30@gmail.com>
2020-08-07 02:06:23 +0200

    merged changes.

--- scripts/word-count-stop.sh
+++ scripts/word-count-stop.sh
@@ -6,8 +6,3 @@ JOB_ID=`cat $HOME/pids/word_count-jid`
 OUTPUT=$($RHINO_HOME/build-target/bin/flink cancel $JOB_ID)
 
 echo $OUTPUT
-
-SAVE_FILE=$(echo $OUTPUT | grep -oP 'file:\K.*')
-SAVE_FILE=${SAVE_FILE::-1}
-
-echo $SAVE_FILE > $HOME/pids/word_count-fid
